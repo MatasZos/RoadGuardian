3e919b65417ddb2029b0dc2fd2d690965646216f
ETHAN2545 <B00164666@mytudublin.ie>
2025-11-24 14:24:16 +0000

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function HomePage() {
   return (
     <main>
-      <h1>Home</h1>
+      <h1 "font-semibold">RoadGuardian</h1>
       <p>
         <Link href="/login">Login here.</Link>
       </p>
