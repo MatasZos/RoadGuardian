64c9da1d04ae0c612de460a738a347ec710b449a
MatasZos <B00163194@mytudublin.ie>
2025-11-24 13:27:54 +0000

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import Link from 'next/link'
 export default function HomePage() {
   return (
     <main>
       <h1>Home</h1>
+    <p>
+    <Link href = "/login"> Login here </Link>
     </main>
   );
 }
