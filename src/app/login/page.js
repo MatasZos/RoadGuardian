67fcda6e4cd615c7048346ba41dfa2ddcefd12bc
MatasZos <B00163194@mytudublin.ie>
2025-11-24 13:34:15 +0000

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -8,6 +8,7 @@ export default function LoginPage() {
           <label htmlFor="email">Email:</label><br />
           <input type="email" id="email" name="email" required />
         </div>
+    <div>
           <label htmlFor="password">Password:</label><br />
           <input type="password" id="password" name="password" required />
         </div>
