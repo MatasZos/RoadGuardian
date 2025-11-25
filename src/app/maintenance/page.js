83e949ea4236e6311a382620728c180c6cc43738
MatasZos <B00163194@mytudublin.ie>
2025-11-25 11:42:10 +0000

    Update page.js

--- src/app/maintenance/page.js
+++ src/app/maintenance/page.js
@@ -1,1 +1,8 @@
-
+export default function MaintenancePage() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Maintenance</h1>
+      <p>Here you can schedule routine motorcycle maintenance or check service history.</p>
+    </main>
+  );
+}
