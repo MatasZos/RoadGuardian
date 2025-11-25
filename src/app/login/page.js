f651550c60681f4191096276e471bd7c81a79757
MatasZos <B00163194@mytudublin.ie>
2025-11-25 11:41:17 +0000

    Update page.js

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,20 +1,21 @@
+"use client"; 
 export default function LoginPage() {
   return (
-    <main>
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>Login</h1>
-
+      <p>Access your motorcycle assistance account here.</p>
       <form>
         <div>
           <label htmlFor="email">Email:</label><br />
           <input type="email" id="email" name="email" required />
         </div>
-
-        <div>
+        <div style={{ marginTop: "1rem" }}>
           <label htmlFor="password">Password:</label><br />
           <input type="password" id="password" name="password" required />
         </div>
-
-        <button type="submit">Login</button>
+        <button type="submit" style={{ marginTop: "1rem" }}>
+          Login
+        </button>
       </form>
     </main>
   );
