1a00e28be85661910faca716faa87ccc7349893e
MatasZos <B00163194@mytudublin.ie>
2025-11-27 12:03:04 +0000

    Update and rename page.js to page.js

---
+++ src/app/documents/page.js
@@ -0,0 +1,8 @@
+export default function InformationPage() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>DOCUMENTS</h1>
+      <p>Here you can schedule routine motorcycle maintenance or check service history.</p>
+    </main>
+  );
+}
