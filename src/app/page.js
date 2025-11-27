9383a308b2341f7e87548b82cae34622169cc999
MatasZos <B00163194@mytudublin.ie>
2025-11-27 11:59:52 +0000

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -78,7 +78,7 @@ export default function HomePage() {
               "&:hover": { bgcolor: "#2fa82e" },
             }}
           >
-            <Link href="/breakdown">ASSISTANCE</Link>
+            <Link href="/breakdown">EMERGENCY</Link>
           </Button>
 
           <Button
@@ -93,7 +93,7 @@ export default function HomePage() {
               "&:hover": { bgcolor: "#cc7a00" },
             }}
           >
-            MAINTENANCE
+            
           </Button>
         </Stack>
       </Box>
