a30f0943233c7f10b4b0e455ac6765ab2bc358e3
Mateusz-gif <b00160065@mytudublin.ie>
2026-01-12 12:20:09 +0000

    Change heading from 'DOCUMENTS' to 'ChatBox'

--- src/app/documents/page.js
+++ src/app/documents/page.js
@@ -1,7 +1,7 @@
 export default function DocumentsPage() {
   return (
     <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>DOCUMENTS</h1>
+      <h1>ChatBox</h1>
       <p>Here you can store all your personal bike related documents.</p>
     </main>
   );
