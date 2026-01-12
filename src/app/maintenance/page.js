356f807114a8f9250624e9a984ad37af2d4ac41d
Mateusz-gif <b00160065@mytudublin.ie>
2026-01-12 13:26:10 +0000

    Update page.js

--- src/app/maintenance/page.js
+++ src/app/maintenance/page.js
@@ -1,17 +1,28 @@
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
 
 export default function MaintenancePage() {
   return (
     <Container maxWidth="md">
       <Box sx={{ py: 4 }}>
-        <Typography variant="h4" sx={{ fontWeight: 700 }}>
-          Maintenance
-        </Typography>
-        <Typography sx={{ mt: 1, opacity: 0.8 }}>
-          Log and review your maintenance records.
-        </Typography>
+        <Paper
+          elevation={2}
+          sx={{
+            p: 4,
+            borderRadius: 4,
+            textAlign: "center",
+          }}
+        >
+          <Typography variant="h4" sx={{ fontWeight: 700, mb: 2 }}>
+            Maintenance
+          </Typography>
+
+          <Typography sx={{ opacity: 0.8 }}>
+            View and manage your motorcycle maintenance records.
+          </Typography>
+        </Paper>
       </Box>
     </Container>
   );
