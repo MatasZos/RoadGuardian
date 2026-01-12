f3a35d8c6743bcfe8f933eec1d5f53141a1c566c
MatasZos <B00163194@mytudublin.ie>
2026-01-12 12:34:12 +0000

    Change heading from 'ChatBox' to 'CDocument Storage'

--- src/app/documents/page.js
+++ src/app/documents/page.js
@@ -1,7 +1,7 @@
 export default function DocumentsPage() {
   return (
     <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>ChatBox</h1>
+      <h1>CDocument Storage</h1>
       <p>Here you can store all your personal bike related documents.</p>
     </main>
   );
