ed5869ac12e73e9c44526959f7b46744f7f3110f
Mateusz-gif <b00160065@mytudublin.ie>
2026-01-12 13:09:43 +0000

    Update maintenance page content

--- src/app/maintenance/page.js
+++ src/app/maintenance/page.js
@@ -2,7 +2,7 @@ export default function MaintenancePage() {
   return (
     <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>Maintenance</h1>
-      <p>Here you can store all your personal bike related documents.</p>
+      <p>Testing</p>
     </main>
   );
 }
